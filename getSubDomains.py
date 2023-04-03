0a08c431711cdbc09875a5c371e77079e7352440
salcedogg <63917314+salcedogg@users.noreply.github.com>
2023-04-02 19:05:05 -0500

    Adding improvements on code
    Mejora de código para la enumeración de subdominios utilizando Python y Google search.

--- getSubDomains.py
+++ getSubDomains.py
@@ -1,21 +1,31 @@
 import requests
 import re
+from googlesearch import search
+
+GOOGLE_SEARCH_URL = "https://www.google.com/search?q=site:{domain}&start=0"
 
 def get_subdomains(domain):
+    try:
+        # Use the googlesearch library to perform the Google search
+        search_query = f"site:{domain}"
+        search_results = search(search_query, num_results=10)
 
-    # Construct the Google search query for the site operator with the specified domain
-    url = f"https://www.google.com/search?q=site:{domain}&start=0"
-    # Set the User-Agent header to a web browser user agent string to avoid being blocked by Google
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
-    # Send an HTTP GET request to the specified URL with the specified headers and get the response
-    response = requests.get(url, headers=headers)
-    # Extract the HTML content of the HTTP response
-    html = response.text
-    # Use a regular expression to search for subdomains in the HTML content of the HTTP response
-    # The regular expression matches domain names that follow the standard format of alphanumeric characters separated by dots, allowing for hyphens in the domain name
-    subdomains = re.findall(r'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]', html)
-    # Return a set of unique subdomains found in the HTML content of the HTTP response
-    return set(subdomains)
+        subdomains = set()
+        for url in search_results:
+            # Send an HTTP GET request to the URL and get the response
+            response = requests.get(url, timeout=5)
+            # Extract the HTML content of the HTTP response
+            http_content = response.text
+            # Use a regular expression to search for subdomains in the HTML content of the HTTP response
+            # The regular expression matches domain names that follow the standard format of alphanumeric characters separated by dots, allowing for hyphens in the domain name
+            subdomains.update(re.findall(r'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]', http_content))
+
+        # Return a set of unique subdomains found in the HTML content of the HTTP responses
+        return subdomains
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return set()
 
 
 # Prompt the user to enter a domain and call the get_subdomains function to enumerate subdomains for the specified domain
@@ -23,14 +33,3 @@ domain = input("Enter the domain to enumerate subdomains for: ")
 subdomains = get_subdomains(domain)
 # Print the resulting set of subdomains
 print(subdomains)
-
- #S4
-
-
-
-
-
-
-
-
-
